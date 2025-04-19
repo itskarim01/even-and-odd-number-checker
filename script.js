5d2fdfdd5213a7b213e663c9bdf79bfeefac2e29
itskarim01 <abbashehu775@gmail.com>
2025-04-19 17:32:29 +0100

    Add HTML, CSS, and JavaScript files for Odd & Even Number Checker application

---
+++ script.js
@@ -0,0 +1,29 @@
+function numChecker() {
+    // Create a variable for collecting the value from the user
+    let number = parseFloat(document.getElementById('number').value)
+
+    // Create a variable for handling the error when the value is empty
+    let xNumber = document.getElementById('number').value
+
+    // Variables for handling the error when the value is a string
+    const noString = xNumber
+    const regEx = /[a-z]/
+    const srtgChecker = regEx.test(noString)
+
+    // Handle input error when the value is empty
+    if (xNumber === '') {
+        document.getElementById('error').innerText = 'Please Enter a Number'
+    }
+    // Handle input error when the user inserted a string
+    else if (srtgChecker === true) {
+        document.getElementById('error').innerText = 'Incorrect Value'
+    }
+    // Compute the given number input to return an even or odd number
+    else if (number % 2 == 0) {
+        document.getElementById('error').innerText = ''
+        alert(`${number} is an EVEN number`)
+    } else {
+        document.getElementById('error').innerText = ''
+        alert(`${number} is an ODD number`)
+    }
+}
