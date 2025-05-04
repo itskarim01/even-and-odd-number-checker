132b2b588ce2827ae282434c1a46e4c601f117bf
itskarim01 <abbashehu775@gmail.com>
2025-05-04 15:00:26 +0100

    Refactor error handling in numChecker function for clarity and efficiency

--- script.js
+++ script.js
@@ -2,20 +2,15 @@ function numChecker() {
     // Create a variable for collecting the value from the user
     let number = parseFloat(document.getElementById('number').value)
 
-    // Create a variable for handling the error when the value is empty
+    // Create a variable for handling errors
     let xNumber = document.getElementById('number').value
-
-    // Variables for handling the error when the value is a string
-    const noString = xNumber
-    const regEx = /[a-z]/
-    const srtgChecker = regEx.test(noString)
 
     // Handle input error when the value is empty
     if (xNumber === '') {
         document.getElementById('error').innerText = 'Please Enter a Number'
     }
     // Handle input error when the user inserted a string
-    else if (srtgChecker === true) {
+    else if (isNaN(xNumber)) {
         document.getElementById('error').innerText = 'Value must be a Number'
     }
     // Compute the given number input to return an even or odd number
